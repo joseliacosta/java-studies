de01c7fc362d562a4e28dccf7e77d4145b2ed397
Josélia L. Costa <joselialcosta@gmail.com>
2018-07-05 16:33:05 -0300

    Create a class to do vertical lines according with user input

---
+++ src/triangles/ex2.java
@@ -0,0 +1,33 @@
+package triangles;
+
+import java.util.Scanner;
+
+public class ex2 {
+    public static void main(String[] args) {
+        /*
+            Draw a vertical line
+
+            Given a number n, print n lines, each with one asterisks
+            Example when n=3:
+            *
+            *
+            *
+        */
+
+        System.out.println("Enter the quantity of lines that you want: ");
+        Scanner scanner = new Scanner(System.in);
+        int linesQtde = scanner.nextInt();
+
+        if(linesQtde > 0) {
+            for(int index = 0; index < linesQtde; index++) {
+                System.out.println("*");
+            }
+        } else {
+            System.out.println("Try a number greater than 0");
+        }
+
+
+
+
+    }
+}
